176ba78af5b7714f5e36a5a191ff9ea24a1a3d76
Sahana2014 <kssahanas@gmail.com>
2025-10-24 18:59:16 +0530

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+#Write a program to calcutate the values of ceil, floor, factorial, sqrt, exp, pi, pow, fabs using math module.
+
+import math  # Importing the math module to access mathematical functions
+print(math.ceil(45.490))  # Calculating and printing the ceiling value of 45.490
+print(math.floor(45.890))  # Calculating and printing the floor value of 45.890
+print(math.factorial(5))  # Calculating and printing the factorial of 5
+print(math.sqrt(49))  # Calculating and printing the square root of 49
+print(math.exp(2))  # Calculating and printing the exponential value of 2
+print(math.pi)  # Printing the value of pi
+print(math.pow(2, 3))  # Calculating and printing 2 raised to the power of 3
+print(math.fabs(-10.5))  # Calculating and printing the absolute value of -10.5 the power of 3
